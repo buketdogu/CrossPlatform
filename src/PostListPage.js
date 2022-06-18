290d36fc4084523256b778b90add2eb6db30c243
buketdogu <34508970+buketdogu@users.noreply.github.com>
2022-06-18 12:13:15 +0300

    first

---
+++ src/PostListPage.js
@@ -0,0 +1,99 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableHighlight,SafeAreaView,Button} from 'react-native';
+
+
+
+export function PostListPage(props) {
+  
+
+    const apiURL = 'https://jsonplaceholder.typicode.com/posts';
+
+    const [postData, setPostData] = useState([]);
+
+
+
+    
+    useEffect(() => {
+
+        fetch(apiURL)
+            .then((res) => res.json())
+            .then((data) => {
+
+                setPostData(data);
+                
+
+            })
+
+    }, [])
+
+  return (
+    <SafeAreaView style={styles.container}>
+        <View style={styles.listContainer}>
+     {postData.map((item, key)=>{
+        if (key < 20) {
+            return (<TouchableHighlight key={key} onPress={()=> props.navigation.navigate("PostDetail", {postid:item.id})}>
+                <Text style={styles.font}>{item.title}</Text> 
+            </TouchableHighlight>)
+            
+        }
+        
+     })}
+    <View style={styles.footer}>
+      <View style={styles.buttonContainer}>
+      <Button onPress={()=> props.navigation.navigate("AlbumList")} title="See Album List" color={"black"}></Button>
+      </View>
+      <View style={styles.buttonContainer}>
+      <Button onPress={()=> props.navigation.navigate("Home")} title="See User List" color={"black"}></Button>
+      </View>
+      <View style={styles.buttonContainer}>
+      <Button onPress={()=> props.navigation.navigate("ToDoList")} title="See ToDo List" color={"black"}></Button>
+      </View>
+      
+      </View>
+      </View>
+        
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    backgroundColor:"#FAF597",
+  },
+  buttonContainer:{
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor:"#2B84F0",
+    width:120,
+   marginLeft:13,
+   borderRadius:10,
+   
+  },
+  listContainer: {
+    display:"flex",
+    marginTop:30,
+    height:600,
+    width:415,
+    
+    
+  },
+  font:{
+  fontWeight:'bold',
+  margin:1,
+  borderWidth:3,
+  
+
+  },
+   footer:{
+    flex:1,
+    flexDirection:"row",
+    justifyContent:"flex-start",
+    alignItems:'center',
+    
+   }
+});
---
+++ src/UserDetailPage.js
@@ -0,0 +1,98 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableHighlight,SafeAreaView, Dimensions} from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+
+
+export function UserDetailPage(props) {
+  
+
+    const apiURL = `https://jsonplaceholder.typicode.com/users/${props.route.params.userid}`;
+
+    const [userData, setuserData] = useState([]);
+   
+    
+    
+    
+    
+    useEffect(() => {
+
+        fetch(apiURL)
+            .then((res) => res.json())
+            .then((data) => {
+
+                setuserData(data);
+                
+
+            })
+
+    }, [])
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        </View>
+        <View style={styles.body}>
+        <Text style={styles.title}>Credential Information</Text>
+      <Text>ID:{userData.id}</Text>
+      <Text>Name:{userData.name}</Text>
+      <Text>Username:{userData.username}</Text>
+     
+      
+        <Text style={styles.title}>Residence Information</Text>
+      <Text>Street:{userData.address?.street}</Text>
+      <Text>Suite:{userData.address?.suite}</Text>
+      <Text>City:{userData.address?.city}</Text>
+      <Text>Zipcode:{userData.address?.zipcode}</Text>
+      
+      
+      <Text style={styles.title}>Geolocation</Text>
+      <Text>Lat:{userData.address?.geo.lat}</Text>
+      <Text>Lng:{userData.address?.geo.lng}</Text>
+      
+      
+      <Text style={styles.title}>Contact Information</Text>
+      <Text>Phone:{userData.phone}</Text>
+      <Text>Website:{userData.website}</Text>
+      <Text>Company Name:{userData.company?.name}</Text>
+      <Text>Catch Phrase:{userData.company?.catchPhrase}</Text>
+      <Text>BS:{userData.company?.bs}</Text>
+      </View>
+      <View style={styles.footer}>
+      </View>
+
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FAF597',
+    width: windowWidth,
+    height: windowHeight,
+    alignItems:"center",
+    justifyContent:"center",
+    
+    
+  },
+  title:{
+    fontWeight:"bold",
+    fontSize: 20,
+    
+  },
+  header:{
+    height: (windowHeight * 10) / 100,
+  },
+  
+  body:{
+    height: (windowHeight * 85) / 100,
+  },
+  footer:{
+    height: (windowHeight * 5) / 100,
+  },
+});
