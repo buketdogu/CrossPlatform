66da7b97847945184b8c57cb4259dd1ac1ad94da
buketdogu <34508970+buketdogu@users.noreply.github.com>
2022-06-22 17:48:12 +0300

    update

--- src/PhotoListPage.js
+++ src/PhotoListPage.js
@@ -2,15 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableHighlight,SafeAreaView,Dimensions,Image,Button} from 'react-native';
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
 
 export function PhotoListPage(props) {
   
 
-    const apiURL = `https://jsonplaceholder.typicode.com/photos/${props.route.params.photoid}`;
+      const apiURL = `https://jsonplaceholder.typicode.com/photos?albumId=` + props.route.params.albumid;
 
-    const [photoData, setPhotoData] = useState([]);
+    const [photoData, setphotoData] = useState([]);
 
 
 
@@ -21,30 +19,41 @@ export function PhotoListPage(props) {
             .then((res) => res.json())
             .then((data) => {
 
-                setPhotoData(data);
+                setphotoData(data);
                 
 
             })
 
-    }, [])
+    })
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        
-      </View>
-      <View style={styles.body}>
-      <Text style={styles.title}>Title: {photoData.title}</Text>
-        <Text>ID: {photoData.id}</Text>
-        <Text>Album ID: {photoData.albumId}</Text>
-        <View>
-        <Image style={{ width: 200, height: 200 }} 
-        source={{ uri: `${photoData.url}` }} />
-        </View>
-        </View>
+    <SafeAreaView style={styles.container}><View style={styles.listContainer}>
+       <FlatList
+                  data={photoData
+                    }
+                  renderItem={({ item }) => (
+                    <>
+                    
+                      <View style={styles.row}>
+                        <Text style={styles.font}>{item.title}</Text>
+                           
+                      </View>
+                       <View style={styles.row}>
+                           <Image style={{ width: 200, height: 100}} 
+                          source={{ uri: `${item.url}` }} /> 
+                      </View>
+                       
+                      
+                      
+                    </>
+                  )}
+                  
+                />
+      
         <View style={styles.footer}>
         <View style={styles.buttonContainer}>
                 <Button onPress={()=> props.navigation.navigate("UserListPage")} title="Return Home" color={"black"}></Button>
+      </View>
       </View>
       </View>
 
@@ -59,34 +68,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FCFBF5',
-    width: windowWidth,
-    height: windowHeight,
+    alignItems:"center",
+    justifyContent:"center",
+    
+  },
+listContainer: {
+    display:"flex",
+    marginTop:30,
+    height:400,
+    width:200,
     
     
   },
-  title:{
-    fontWeight:"bold",
-    fontSize: 15,
-    marginBottom:30,
-    
-  },
-  header:{
-    height: (windowHeight * 5) / 100,
+  font:{
+  fontWeight:'bold',
+  margin:5,
+  borderWidth:3,
+  
+
   },
 
   buttonContainer:{
   justifyContent:"center",
    alignItems:"center",
    width:120,
+   marginTop: 100,
   borderRadius:10,
  },
   
-  body:{
-    height: (windowHeight * 75) / 100,
-    alignItems:"center",
-    justifyContent:"flex-start",
-  },
+ 
   footer:{
-    height: (windowHeight * 5) / 100,
+    height: 65,
   },
 });
